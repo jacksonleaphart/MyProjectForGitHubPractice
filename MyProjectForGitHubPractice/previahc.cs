f5b1d810ed5973c2194d9da18c066011921b9b27
Heitor Previatti <previahc@mail.uc.edu>
2024-04-02 11:59:42 -0400

    Create new class with my 6+2

---
+++ MyProjectForGitHubPractice/previahc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectForGitHubPractice
+{
+    internal class previahc
+    {
+        public previahc()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
